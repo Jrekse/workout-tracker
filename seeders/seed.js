9a4efd794fe7945a3fc8c8086dee6579342ee87e
Jon Ekse <jrekse@gmail.com>
2021-06-01 14:26:23 -0700

    seed thing

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/fitnessTracker", {
+mongoose.connect("mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
